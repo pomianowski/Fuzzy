fe369744238b029580390d86bf3c7a408720e5df
Pomianowski <kontakt@rapiddev.pl>
2021-01-03 16:58:09 +0100

    Custom navigation

---
+++ FuzzyNumbers/Assets/Controls/WindowNavigation.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FuzzyNumbers.Assets.Controls
+{
+    /// <summary>
+    /// Interaction logic for WindowNavigation.xaml
+    /// </summary>
+    public partial class WindowNavigation : UserControl
+    {
+        public WindowNavigation()
+        {
+            InitializeComponent();
+        }
+
+        private void AppBarButton(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Tag.ToString())
+            {
+                case "minimize":
+                    App.Current.MainWindow.WindowState = WindowState.Minimized;
+                    break;
+                case "maximize":
+                    this.Maximize();
+                    break;
+                case "close":
+                    App.Current.Shutdown();
+                    break;
+            }
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                App.Current.MainWindow.DragMove();
+        }
+
+        private void DragMaximize(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+                this.Maximize();
+        }
+
+        private void Maximize()
+        {
+            if (App.Current.MainWindow.WindowState == WindowState.Normal)
+            {
+                App.Current.MainWindow.ResizeMode = ResizeMode.NoResize;
+                MaximizeButton.Style = (Style)App.Current.Resources["RestoreButton"];
+                App.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                App.Current.MainWindow.ResizeMode = ResizeMode.CanResize;
+                MaximizeButton.Style = (Style)App.Current.Resources["MaximizeButton"];
+                App.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+        }
+    }
+}
--- FuzzyNumbers/Views/Container.xaml.cs
+++ FuzzyNumbers/Views/Container.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Navigation;
 
 namespace FuzzyNumbers.Views
 {
@@ -19,9 +8,31 @@ namespace FuzzyNumbers.Views
     /// </summary>
     public partial class Container : Window
     {
+        public enum Pages
+        {
+            Dashboard = 0
+        }
         public Container()
         {
             InitializeComponent();
+            this.Navigate(Pages.Dashboard);
+        }
+
+        public void Navigate(Pages page)
+        {
+            switch (page)
+            {
+                default:
+                    rootFrame.Navigate(new Views.Pages.Dashboard());
+                    break;
+            }
+        }
+
+        private void FrameOnNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+            if (e.Content == null)
+                return;
+            rootFrame.NavigationService.RemoveBackEntry();
         }
     }
 }

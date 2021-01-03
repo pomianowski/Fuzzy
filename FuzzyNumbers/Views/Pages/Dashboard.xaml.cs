ebeec182fed5fbcad7eec8e616d88e55502db11d
Pomianowski <kontakt@rapiddev.pl>
2021-01-03 18:15:09 +0100

    Add cartesian charts

--- FuzzyNumbers/Views/Pages/Dashboard.xaml.cs
+++ FuzzyNumbers/Views/Pages/Dashboard.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace FuzzyNumbers.Views.Pages
 {
@@ -8,9 +10,35 @@ namespace FuzzyNumbers.Views.Pages
     /// </summary>
     public partial class Dashboard : Page
     {
+        public SeriesCollection SeriesCollection { get; set; }
         public Dashboard()
         {
             InitializeComponent();
+            this.InitializeChart();
+        }
+
+        private void InitializeChart()
+        {
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Fuzzy number #1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                },
+                new LineSeries
+                {
+                    Title = "Fuzzy number #2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+                },
+                new LineSeries
+                {
+                    Title = "Result",
+                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                }
+            };
+
+            DataContext = this;
         }
 
         private void Button_Calc(object sender, RoutedEventArgs e)

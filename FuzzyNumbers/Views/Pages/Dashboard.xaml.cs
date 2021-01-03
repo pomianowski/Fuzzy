7586d8b24b05514e416430d856e110faccce964e
Pomianowski <kontakt@rapiddev.pl>
2021-01-03 19:47:38 +0100

    Dashboard methods

--- FuzzyNumbers/Views/Pages/Dashboard.xaml.cs
+++ FuzzyNumbers/Views/Pages/Dashboard.xaml.cs
@@ -24,21 +24,33 @@ namespace FuzzyNumbers.Views.Pages
                 new LineSeries
                 {
                     Title = "Fuzzy number #1",
+                    StrokeThickness = 1,
                     Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
                 },
                 new LineSeries
                 {
                     Title = "Fuzzy number #2",
+                    StrokeThickness = 1,
                     Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
                 },
                 new LineSeries
                 {
                     Title = "Result",
+                    StrokeThickness = 1,
                     Values = new ChartValues<double> { 4,2,7,2,7 },
                 }
             };
 
             DataContext = this;
+        }
+
+        private void TextBoxInputFuzzy_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            string tag = (sender as TextBox).Tag.ToString().ToLower().Trim();
+            string value = (sender as TextBox).Text;
+#if DEBUG
+            System.Diagnostics.Debug.WriteLine("Key up (" + tag + "), value is: " + value);
+#endif
         }
 
         private void Button_Calc(object sender, RoutedEventArgs e)

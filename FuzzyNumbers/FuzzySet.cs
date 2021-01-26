52d6e66523d8c3560a1af7e949f8ff8871118469
Pomianowski <kontakt@rapiddev.pl>
2021-01-26 22:26:41 +0100

    Update Fuzzy class

---
+++ FuzzyNumbers/FuzzySet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyNumbers
+{
+
+    enum CalcType
+    {
+        Singleton = 0,
+        Gamma,
+        L,
+        T
+    }
+
+    struct FuzzyValue
+    {
+        public double x { get; set; }
+        public double value { get; set; }
+    }
+
+    class FuzzySet
+    {
+        public CalcType Type;
+
+        public FuzzyValue a;
+        public FuzzyValue b;
+        public FuzzyValue c;
+
+        public FuzzySet Product(FuzzySet set)
+        {
+            return set;
+        }
+
+        public FuzzySet Sum(FuzzySet set)
+        {
+            return set;
+        }
+
+        public FuzzySet Adjunct(FuzzySet set)
+        {
+            return set;
+        }
+    }
+}
--- FuzzyNumbers/Views/Pages/Dashboard.xaml.cs
+++ FuzzyNumbers/Views/Pages/Dashboard.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using LiveCharts;
+using LiveCharts.Helpers;
 using LiveCharts.Wpf;
 
 namespace FuzzyNumbers.Views.Pages
@@ -25,21 +26,65 @@ namespace FuzzyNumbers.Views.Pages
                 {
                     Title = "Fuzzy number #1",
                     StrokeThickness = 1,
-                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                    LineSmoothness = 0,
+                    PointGeometry = null,
+                    Fill = System.Windows.Media.Brushes.Transparent,
+                    DataLabels = false,
+                    Values = new ChartValues<double> { double.NaN, double.NaN, double.NaN, double.NaN, 0, 1, 1, 1, 1, 1, 1, 1, 0 }
                 },
                 new LineSeries
                 {
                     Title = "Fuzzy number #2",
                     StrokeThickness = 1,
-                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+                    LineSmoothness = 0,
+                    Fill = System.Windows.Media.Brushes.Transparent,
+                    PointGeometry = null,
+                    DataLabels = false,
+                    Values = new ChartValues<double> { double.NaN, double.NaN, double.NaN, double.NaN,  double.NaN, 0, 1, 1, 1, 1, 1 , 0 },
                 },
                 new LineSeries
                 {
-                    Title = "Result",
+                    Title = "Fuzzy number #3",
                     StrokeThickness = 1,
-                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                    LineSmoothness = 0,
+                    Fill = System.Windows.Media.Brushes.Transparent,
+                    PointGeometry = null,
+                    DataLabels = false,
+                    Values = new ChartValues<double> { double.NaN, double.NaN, double.NaN, double.NaN,  double.NaN, 0, 1, 1, 1, 1, 0, double.NaN },
+                },
+                new LineSeries
+                {
+                    Title = "Fuzzy number #4",
+                    StrokeThickness = 1,
+                    LineSmoothness = 0,
+                    PointGeometry = null,
+                    Fill = System.Windows.Media.Brushes.Transparent,
+                    DataLabels = false,
+                    Values = new ChartValues<double> { double.NaN, 0, 1, double.NaN,  double.NaN, double.NaN, double.NaN, double.NaN, double.NaN, double.NaN, double.NaN, double.NaN },
+                },
+                new LineSeries
+                {
+                    Title = "Fuzzy number #5",
+                    StrokeThickness = 1,
+                    LineSmoothness = 0,
+                    Fill = System.Windows.Media.Brushes.Transparent,
+                    PointGeometry = null,
+                    DataLabels = false,
+                    Values = new ChartValues<double> { double.NaN, 1, 0, 0,  1, double.NaN, double.NaN, double.NaN, double.NaN, double.NaN, double.NaN, double.NaN },
                 }
             };
+
+            cartesianChart.AxisY.Add(new Axis
+            {
+                MinValue = 0,
+                MaxValue = 1,
+                Separator = new LiveCharts.Wpf.Separator
+                {
+                    StrokeThickness = 1,
+                    Step = 1
+                }
+
+            });
 
             DataContext = this;
         }
@@ -63,6 +108,43 @@ namespace FuzzyNumbers.Views.Pages
             switch (tag)
             {
                 default:
+                    break;
+            }
+        }
+
+        private void Button_AddNew(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboClass_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = (sender as ComboBox).SelectedIndex;
+
+
+            if (textInputOne == null || textInputTwo == null || textInputThree == null)
+                return;
+
+
+            switch (index)
+            {
+                case 0:
+                    textInputOne.IsEnabled = true;
+                    textInputTwo.IsEnabled = textInputThree.IsEnabled = false;
+                        break;
+
+                case 1:
+                    textInputOne.IsEnabled = textInputTwo.IsEnabled = true;
+                    textInputThree.IsEnabled = false;
+                    break;
+
+                case 2:
+                    textInputOne.IsEnabled = textInputTwo.IsEnabled = true;
+                    textInputThree.IsEnabled = false;
+                    break;
+
+                case 3:
+                    textInputOne.IsEnabled = textInputTwo.IsEnabled = textInputThree.IsEnabled = true;
                     break;
             }
         }
